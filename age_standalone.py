fe42a5e5f5e1ee5dec10aa91991ae6e3fadd0071
robomanModding <nathanr11.ingham@outlook.com>
2026-05-03 23:48:55 +0100

    Create age_standalone.py

---
+++ age_standalone.py
@@ -0,0 +1,85 @@
+#--------Get data--------
+
+from time import localtime
+year = localtime().tm_year
+month = localtime().tm_mon
+day = localtime().tm_mday
+
+#-----------Prep---------
+
+def getDay():
+    global leap
+    leap = 'false'
+    global monDays
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        monDays = 31
+
+    if month == 2:
+        leap = (year - 2000) / 4
+        if not isinstance(leap, int):
+            monDays = 28
+            leap = 'false'
+        else:
+            monDays = 29
+            leap = 'true'
+
+    if month in (4, 6, 9, 11):
+        monDays = 30
+    print(f'Year: {year}')
+    print(f'Month: {month}')
+    print(f'Day: {day}')
+    print(f'Leap: {leap}')
+    print(f'This month has {monDays} days')
+
+#--------Get user data----------
+
+getDay()
+bday = ''
+bmon = ''
+birth_year = ''
+while bday == '':
+    try:
+        bday = int(input('What day were you born on? '))
+    except ValueError:
+        print('Numbers please! (Make sure to just write the DAY not the full date)')
+while bmon == '':
+    try:
+        bmon = int(input('Month? '))
+    except ValueError:
+        print('*sigh* NUMBERSSSSS!')
+while birth_year == '':
+    try:
+        birth_year = int(input('Year? '))
+    except ValueError:
+        print('wtf? WHO EVEN WRITES THE YEAR IN WORDS???')
+yearAge = year - birth_year
+monAge = month - bmon
+dayAge = day - bday
+
+#-Determine whether or not the month should be classed as before your birthday or after and doing maths to use that and output
+
+if month > bmon:
+    if day > bday:
+        actualAge = f'You are {yearAge} years, {monAge} months and {dayAge} days old'
+    else:
+        month = bmon
+        getDay()
+        actualAge = f'You are {yearAge} years, {monAge} months and {(monDays - bday) + day} days old'
+else:
+    if (12 - bmon) + month == 12:
+        actMonth = 0
+        actY = yearAge
+    else:
+        actMonth = (12 - bmon) + month
+        actY = yearAge - 1
+    if day > bday:
+        actualAge = f'You are {actY} years, {actMonth} months and {dayAge} days old'
+    else:
+        month = bmon
+        getDay()
+        actualAge = f'You are {actY} years, {actMonth - 1} months and {((monDays - bday) + day) + 1} days old'
+
+
+#Tell the user how old they are :)
+
+print(actualAge)
